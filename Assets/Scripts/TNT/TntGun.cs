a51c2738d0a0234d1fd885a0b7a5e04f3a5dc24a
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-11-29 02:06:57 +0300

    Add initial velocity to TNTs

--- Assets/Scripts/TNT/TntGun.cs
+++ Assets/Scripts/TNT/TntGun.cs
@@ -7,6 +7,7 @@ namespace TNT
 		[SerializeField] private Tnt _tntPrefab = default;
 		[SerializeField, Min(0f)] private float _height = 20f;
 		[SerializeField, Min(0f)] private float _maxDistance = 100f;
+		[SerializeField] private Vector3 _initialVelocity = Vector3.zero;
 
 		private void Update()
 		{
@@ -15,7 +16,8 @@ namespace TNT
 			var direction = _camera.ScreenPointToRay(Input.mousePosition);
 			if (!Physics.Raycast(direction, out var hit, _maxDistance)) return;
 
-			Instantiate(_tntPrefab, hit.point + Vector3.up * _height, Quaternion.identity);
+			var tnt = Instantiate(_tntPrefab, hit.point + Vector3.up * _height, Quaternion.identity);
+			tnt.GetComponent<Rigidbody>().velocity = _initialVelocity;
 		}
 
 		private void Awake()
